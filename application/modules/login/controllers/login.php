bf8e553190e9786c8d29ac3112097882359a605b
Mohindar <mohindarv@gmail.com>
2020-04-01 15:57:29 +0530

    Code Changes

--- application/modules/login/controllers/login.php
+++ application/modules/login/controllers/login.php
@@ -12,65 +12,64 @@ class Login extends MY_Controller
         $this->load->library("session");
   }
 
-    public function index()
+  public function index()
+  {
+    $this->load->view('login');
+  }
+   
+  public function userCheck()
+  {
+    $this->form_validation->set_rules('email', 'Email', 'required|valid_emails|valid_email');
+    $this->form_validation->set_rules('password', 'Password', 'required');
+
+    if ($this->form_validation->run() == FALSE)
     {
-        $this->load->view('login');
-    }
-    public $sessionData;
-    public function userCheck()
-    {
-      
-      $this->form_validation->set_rules('email', 'Email', 'required|valid_emails|valid_email');
-      $this->form_validation->set_rules('password', 'Password', 'required');
-
-        if ($this->form_validation->run() == FALSE)
-        {
-            $this->load->view('login');
-        }
-        else
-        {
-          $getData = array(            
-            'email' => $this->input->post('email')
-          );
-
-          $data = $this->Logins->getuserDetails($getData);
-
-          foreach ($data as $value)
-          {
-            $name     = $value['name'];
-            $email    = $value['email'];
-            $password = $value['password'];
-            $profileimage = $value['profileimage'];
-          }
-
-          $useremail    = $this->input->post('email');
-          $userpassword = $this->input->post('password');
-          $userpassword = md5($userpassword);
-
-          if ($userpassword == $password && $useremail == $email)
-          {
-            $sessionData = array(
-              'name'     => $name,
-              'email'    => $email,
-              'password' => $password,
-              'profileimage' => $profileimage,
-            );
-            $this->session->set_userdata($sessionData);
-
-            redirect(site_url('dashboard/index'));
-          }
-          else
-          {
-            $this->session->set_flashdata('error', 'email or password is incorrect!');
-            $this->load->view('login');
-          }
-        }
-    }
-
-    public function logout($value='')
-    {
-      $this->session->sess_destroy();
       $this->load->view('login');
     }
+    else
+    {
+      $getData = array(            
+         'email' => $this->input->post('email')
+      );
+
+      $data = $this->Logins->getuserDetails($getData);
+
+      foreach ($data as $value)
+      {
+        $name     = $value['name'];
+        $email    = $value['email'];
+        $password = $value['password'];
+        $profileimage = $value['profileimage'];
+      }
+
+      $useremail    = $this->input->post('email');
+      $userpassword = $this->input->post('password');
+      $userpassword = md5($userpassword);
+
+      if ($userpassword == $password && $useremail == $email)
+      {
+        $sessionData = array(
+            'name'     => $name,
+            'email'    => $email,
+            'password' => $password,
+            'profileimage' => $profileimage,
+        );
+
+        $this->session->set_userdata($sessionData);
+        redirect(site_url('dashboard/index'));
+      }
+      else
+      {
+        $this->session->set_flashdata('error', 'email or password is incorrect!');
+        $this->load->view('login');
+      }
+    }
+  }
+
+  public function logout($value='')
+  {
+    $this->session->sess_destroy();
+    $this->load->view('login');
+  }
 
 }
